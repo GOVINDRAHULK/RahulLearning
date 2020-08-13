87848d40b4c7695a0031f1b5535c6457c88cd7f3
GOVIND RAHUL K <govind.rahul.kantheti@signify.com>
2020-08-13 12:05:21 +0530

    Abstraction

---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Abstraction
+{
+    abstract class R
+    {
+        public R()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+        public void a()
+        {
+            Console.WriteLine("Method a");
+        }
+        public abstract void b();
+    }
+    class Ra : R
+    {
+        public override void b()
+        {
+            Console.WriteLine("Ra : Overided Abstract Function");
+        }
+    }
+
+    class Rb : R
+    {
+        public override void b()
+        {
+            Console.WriteLine("Rb : Overided Abstract Function");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ra ra = new Ra();
+            ra.a();
+            ra.b();
+            Rb rb = new Rb();
+            rb.a();
+            rb.b();
+        }
+    }
+}
