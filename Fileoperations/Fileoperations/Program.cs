fdcb5a5d2a1297be34fb2009d6d853a062285af8
GOVIND RAHUL K <govind.rahul.kantheti@signify.com>
2020-08-19 16:40:44 +0530

    File Operations

--- Fileoperations/Fileoperations/Program.cs
+++ Fileoperations/Fileoperations/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Fileoperations
 {
@@ -7,6 +8,49 @@ namespace Fileoperations
     {
         static void Main(string[] args)
         {
+            //File Class
+
+            try
+            {
+                string path = @"C:\Users\RAHUL\Desktop\fileinput.txt";
+                if (File.Exists(path)) File.Delete(path);
+                //Using file stream to create file
+                using (FileStream fs = File.Create(path))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes("This is Testing");
+                    fs.Write(info, 0, info.Length);
+                }
+
+                //Using file stream to read file
+                using (FileStream fs = File.OpenRead(path))
+                {
+                    byte[] b = new byte[1024];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+                    while (fs.Read(b, 0, b.Length) > 0)
+                    {
+                        Console.WriteLine(temp.GetString(b));
+                    }
+                }
+                
+
+                //Using StreamWriter to create file
+                if (File.Exists(path)) File.Delete(path);
+                StreamWriter sw = File.CreateText(path);
+                sw.WriteLine("Testing from StreamWriter");
+                sw.Close();
+
+                string[] rt = File.ReadAllLines(path);
+                foreach (string s in rt)
+                {
+                    Console.WriteLine(s);
+                }
+                string rt2 = File.ReadAllText(path);
+                Console.WriteLine(rt2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             // Data writing into file and reading from file using FileStream
             try
             {
